3d2bfc6efc2401fc9cdbb1703cf4b027383d7e36
gonenbilgiislem <34443353+gonenbilgiislem@users.noreply.github.com>
2023-01-16 01:56:25 +0300

    Exceptionlarlı hali

--- tahakkuk2.sql
+++ tahakkuk2.sql
@@ -1,11 +1,10 @@
-/* Formatted on 15/01/2023 22:58:37 (QP5 v5.388) */
 DECLARE
     CURSOR Cr_Bey IS
         SELECT B.*
         FROM Gys_Beyan B
         WHERE B.modul_kod = 1
           AND B.mukellef_bitis_tarih IS NULL
-          AND B.KISI_KOD BETWEEN '800920' AND '800920'
+          AND B.KISI_KOD BETWEEN '80920' AND '80920'
           AND B.ADA = 338
           AND B.PARSEL = 12
           AND KOD NOT IN
@@ -20,14 +19,18 @@ DECLARE
     V_Islem_Yapildi_Eh VARCHAR2(1);
     X_TY_GEN_CEVAP     TY_GEN_CEVAP;
     X_Islem_Kod        Gys_Tahakkuk_Ana.Islem_Kod%TYPE;
+    X_COUNT            NUMBER;
 BEGIN
     FOR Rc_Bey IN Cr_Bey
         LOOP
             X_TY_GEN_CEVAP := TY_GEN_CEVAP();
             V_Islem_Yapildi_Eh := 'E';
             X_Islem_Kod := 'II_SINIF_TERKINI';
+            X_COUNT := SQL%ROWCOUNT;
 
-         --   EXECUTE IMMEDIATE 'ALTER TRIGGER TRG_GYS_TAHAKKUK_LOG DISABLE';
+
+                              EXECUTE IMMEDIATE 'ALTER TRIGGER TRG_GYS_TAHAKKUK_LOG DISABLE';
+
 
             PG_GYS_EMLAK_SERVIS.Sp_Emlak_Tahakkuk(
                     'I',
@@ -46,52 +49,33 @@ BEGIN
                     27,
                     X_TY_GEN_CEVAP);
 
+
             IF X_TY_Gen_Cevap.Kod IS NOT NULL
                 AND X_TY_Gen_Cevap.Aciklama IS NOT NULL
             THEN
-                DBMS_OUTPUT.PUT_LINE('Kod: ' || X_TY_Gen_Cevap.Kod || ' Aciklama: ' || X_TY_Gen_Cevap.Aciklama);
-                DBMS_OUTPUT.PUT_LINE('SQLCODE-1_1: ' || SQLCODE);
-                DBMS_OUTPUT.PUT_LINE('SQLERRM-1_2: ' || SQLERRM);
+                DBMS_OUTPUT.PUT_LINE('Kod1: ' || X_TY_Gen_Cevap.Kod || ' Aciklama1: ' || X_TY_Gen_Cevap.Aciklama ||
+                                     'Parametre veya Log Trigger Kontrol Ediniz.!');
                 ROLLBACK;
-            ELSE
                 IF V_Islem_Yapildi_Eh = 'E'
                 THEN
-                    DBMS_OUTPUT.PUT_LINE('Kod: ' || X_TY_Gen_Cevap.Kod || ' Aciklama: ' || X_TY_Gen_Cevap.Aciklama);
-                    DBMS_OUTPUT.put_line('Alinan Hata Kodu : ' || SQLCODE);
-                    DBMS_OUTPUT.put_line('Alinan Hata Mesaji : ' || SQLERRM);
                     COMMIT;
-
+                    DBMS_OUTPUT.PUT_LINE(X_COUNT);
                     EXECUTE IMMEDIATE 'ALTER TRIGGER TRG_GYS_TAHAKKUK_LOG ENABLE';
                 ELSE
-                    DBMS_OUTPUT.PUT_LINE('Kod: ' || X_TY_Gen_Cevap.Kod || ' Aciklama: ' || X_TY_Gen_Cevap.Aciklama);
-                    DBMS_OUTPUT.PUT_LINE('SQLCODE-2_1: ' || SQLCODE);
-                    DBMS_OUTPUT.PUT_LINE('SQLERRM-2_2: ' || SQLERRM);
+                    DBMS_OUTPUT.PUT_LINE('Kod2: ' || X_TY_Gen_Cevap.Kod || ' Aciklama2: ' || X_TY_Gen_Cevap.Aciklama);
                     ROLLBACK;
                 END IF;
             END IF;
+                DBMS_OUTPUT.PUT_LINE(X_COUNT);
         END LOOP;
-    DBMS_OUTPUT.put_line('Alinan Hata Kodu : ' || SQLCODE);
-    DBMS_OUTPUT.put_line('Alinan Hata Mesaji : ' || SQLERRM);
+    DBMS_OUTPUT.PUT_LINE('Kod3: ' || X_TY_Gen_Cevap.Kod || ' Aciklama3: ' || X_TY_Gen_Cevap.Aciklama);
+    DBMS_OUTPUT.PUT_LINE(X_COUNT);
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Kod4: ' || X_TY_Gen_Cevap.Kod || ' Aciklama4: ' || X_TY_Gen_Cevap.Aciklama);
 EXCEPTION
-    WHEN NO_DATA_FOUND
-        THEN
-            DBMS_OUTPUT.put_line('Alinan Hata Kodu : ' || SQLCODE);
-            DBMS_OUTPUT.put_line('Alinan Hata Mesaji : ' || SQLERRM);
-    WHEN OTHERS
-        THEN
-            DBMS_OUTPUT.put_line('ERROR!');
-
-
-            --DBMS_OUTPUT.PUT_LINE ('Kod: '|| X_TY_Gen_Cevap.Kod|| ' Aciklama: '|| X_TY_Gen_Cevap.Aciklama);
---                DBMS_OUTPUT.PUT_LINE ('SQLCODE-2_1: ' || SQLCODE);
---                DBMS_OUTPUT.PUT_LINE ('SQLERRM-2_2: ' || SQLERRM);
-            COMMIT;
-    --EXCEPTION
---    WHEN NO_DATA_FOUND
---    THEN
---        DBMS_OUTPUT.put_line ('Alinan Hata Kodu : ' || SQLCODE);
---        DBMS_OUTPUT.put_line ('Alinan Hata Mesaji : ' || SQLERRM);
---    WHEN OTHERS
---    THEN
---        DBMS_OUTPUT.put_line ('ERROR!');
+    WHEN NO_DATA_FOUND THEN
+        dbms_output.put_line('Alinan Hata Kodu : ' || sqlcode);
+        dbms_output.put_line('Alinan Hata Mesaji : ' || sqlerrm);
+    WHEN OTHERS THEN
+        dbms_output.put_line('ERROR!');
 END;
